87a97ac529ddde072bdda8342a14f1b1dfa3c640
heloisa <helozacardi@gmail.com>
2025-03-31 13:44:38 -0300

    while comando break

---
+++ teste.py
@@ -0,0 +1,7 @@
+numero = 0
+while True: # Loop infinito
+    numero = numero + 1
+    print(numero)
+    if numero == 5:
+        break # sai do loop
+        
---
+++ while02.py
@@ -0,0 +1,4 @@
+x = 5
+while x >=  0:
+    print('Loop infinito!')
+    x -= 1
